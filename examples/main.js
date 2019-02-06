916c29352873423b8aca8899671f99e3c1cf402d
Mhammad1704 <natgeo1416@hotmail.com>
2019-02-06 21:30:32 +0300

    I used jQuery !

--- examples/main.js
+++ examples/main.js
@@ -3,3 +3,27 @@
 
 // method: get
 // url: https://dog.ceo/api/breed/boxer/images/random
+function printStuff (response2){ //response2 can be any thing
+    let img = document.createElement('img');
+    img.src = response2.data.message;
+    document.body.appendChild(img);
+console.log(response2.data.message)
+}
+function altrnativ(response3){
+
+    let img = document.createElement('img');
+    img.src = response3.data.message;
+    document.body.appendChild(img);
+console.log(response3.data.message)
+
+    console.log('soooory')
+}
+axios({
+    method : 'get',
+    url: 'https://geek-jokes.sameerkumar.website/api',
+
+    // method : 'get',
+    // url: 'https://purr.objects-us-east-1.dream.io/i/0WUGa.jpg'
+})
+.then(altrnativ)
+.catch(printStuff);
